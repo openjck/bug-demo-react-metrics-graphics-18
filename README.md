50fb3c223f4bd0e9c7e8e5af4356278cad22168c
John Karahalis <john.karahalis@gmail.com>
2018-02-07 22:12:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This repo demonstrates [react-metrics-graphics issue #18](https://github.com/mozilla/react-metrics-graphics/issues/18).
+
+## Steps to reproduce
+
+1. Clone this repo
+2. `yarn`
+3. `yarn build`
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ReactMetricsGraphics from 'react-metrics-graphics';
+
+
+function App() {
+    return null;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
